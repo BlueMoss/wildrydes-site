b4da049c0126694d756b1b6c6341d122410d7154
root <root@ip-172-31-43-184.ap-northeast-2.compute.internal>
2025-02-27 02:56:32 +0000

    ver1.4

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_K1lnqAFmQ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '68lk0fnejjcmk0sq12o5o98u8m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5v0eol7f3vsibma0r1to7cec3c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
