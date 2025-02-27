ec54aad20c3e7bc33276b1423bf80af8e2a92c92
root <root@ip-172-31-32-101.ap-northeast-2.compute.internal>
2025-02-27 06:54:15 +0000

    ver3.1

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_YWAVpr34R', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5v0eol7f3vsibma0r1to7cec3c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1clua3ga6kisjek8d9gpibfmpp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://c7sg2vskuk.execute-api.ap-northeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://master.d33ae8k071l2j0.amplifyapp.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
